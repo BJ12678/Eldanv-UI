773c146aa69c38cb546bff270356a374f913931a
BJ12678 <ernestobilerjr@gmail.com>
2020-12-10 10:31:23 +0800

    modify pages

--- src/views/Station.vue
+++ src/views/Station.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <NavMenu />
+    <StationComponent />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import NavMenu from "@/components/NavMenu.vue";
+import StationComponent from "@/components/StationComponent.vue";
 
 export default {
   name: "Station",
   components: {
-    NavMenu
+    NavMenu,
+    StationComponent
   }
 };
 </script>
