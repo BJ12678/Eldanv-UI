08d988f0586181350d6db956dbd52a4832b17410
BJ12678 <ernestobilerjr@gmail.com>
2020-12-10 10:34:10 +0800

    modify components

---
+++ src/store/modules/employee.js
@@ -0,0 +1,21 @@
+// import router from "@/router";
+
+const state = () => ({
+  employee: {
+    name: "",
+    sale: "",
+    container: "",
+    customers: []
+  }
+});
+const getters = {};
+const actions = {};
+const mutations = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+};
